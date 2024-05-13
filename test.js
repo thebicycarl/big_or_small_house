2af36e589bd822c294757b9c900efc6b67c3581e
CarlHockey <thebicycarl@gmail.com>
2024-05-13 16:12:40 +1000

    update

--- test.js
+++ test.js
@@ -1,21 +1,27 @@
-let deposit = document.getElementById('deposit')
-let calcButton = document.getElementById('runBtn')
-let outputText = document.getElementById('eTime')
-let testValue = 0;
+// let deposit = document.getElementById('deposit')
+// let calcButton = document.getElementById('runBtn')
+// let outputText = document.getElementById('eTime')
+// let testValue = 0;
 
-calcButton.addEventListener('click', testRun)
+// calcButton.addEventListener('click', testRun)
 
-function testRun() {
-    let depositv = deposit.value
-    testValue = depositv * 2
-    console.log(deposit.value)
-    console.log(depositv)
+// function testRun() {
+//     let depositv = deposit.value
+//     testValue = depositv * 2
+//     console.log(deposit.value)
+//     console.log(depositv)
 
 
-    outputText.innerHTML=`${depositv}`
-    run2()
-}
+//     outputText.innerHTML=`${depositv}`
+//     run2()
+// }
 
-function run2() {
-    console.log(testValue)
-}
+// function run2() {
+//     console.log(testValue)
+// }
+
+
+console.log(235 % 10)
+console.log(123 % 10)
+console.log(22 % 10)
+console.log(4710 % 10)
